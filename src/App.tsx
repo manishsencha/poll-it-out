b9f0181354dbe13cfabf779082557b08bcb5434d
Manish Sencha <manishsencha2001@gmail.com>
2021-04-06 22:29:45 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Here will be our layout! This is initial setup without any components
+        yet, but will do and also this is first time for me to use typescript !
+        Gonna be fun !!
+      </h1>
     </div>
   );
 }
